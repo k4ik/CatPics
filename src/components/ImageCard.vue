0df29ee5fee8c9a6d08e1dd74afd220be72cb203
k4ik <kaikleal1234@gmail.com>
2024-01-07 15:59:55 -0300

    feat: create ImageCard Component

---
+++ src/components/ImageCard.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="max-w-80 flex flex-col items-center bg-emerald-500 pt-4">
+        <img :src="src" alt="image cat" class="w-72 h-72">
+        <form @submit.prevent>
+            <button class="h-auto px-4 py-1 my-2 rounded-lg bg-blue-400">Generate pictures</button>
+        </form>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "ImageCard",
+        props: ["src"]
+    }
+</script>
