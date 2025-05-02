40740c878ebb9b3fb1fe18d8ee7712764ee4e804
k4ik <kaikleal1234@gmail.com>
2025-05-02 13:19:33 -0300

    feat: create SoundButton component

---
+++ frontend/src/components/SoundButton.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { ref, nextTick } from 'vue';
+const audio = ref(null);
+const audioSource = ref('');
+
+const paths = [
+  new URL("../assets/audio/cat1.wav", import.meta.url).href,
+  new URL("../assets/audio/cat2.wav", import.meta.url).href,
+  new URL("../assets/audio/cat3.wav", import.meta.url).href,
+  new URL("../assets/audio/cat4.wav", import.meta.url).href,
+  new URL("../assets/audio/cat5.wav", import.meta.url).href
+];
+
+const playSound = async () => {
+  const random = Math.floor(Math.random() * paths.length);
+  audioSource.value = paths[random];
+  await nextTick();
+  audio.value.play();
+
+  console.log('button')
+};
+</script>
+
+<template>
+  <button @click="playSound" class="bg-[var(--bazaar)] w-[54px] h-[54px] rounded-full flex items-center justify-center hover:bg-[var(--kabul)] transition duration-400 ease-in-out">
+    <img src="../assets/images/pets.png" alt="cat paw" />
+  </button>
+
+  
+  <audio :src="audioSource" ref="audio" />
+  </template>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <script setup>
-
+import SoundButton from '../components/SoundButton.vue';
 </script>
 
 <template>
-
+  <SoundButton />
 </template>
