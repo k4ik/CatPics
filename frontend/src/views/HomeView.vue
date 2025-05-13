5437d7ea7c09fb003509cc6d106ef676e03c810d
k4ik <kaikleal1234@gmail.com>
2025-05-13 10:51:38 -0300

    feat: create getPictures function

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,17 +1,40 @@
 <script setup>
+import { ref, onMounted } from 'vue';
 import SoundButton from '../components/SoundButton.vue';
+import Message from '../components/Message.vue';
+
+const image = ref('')
+const message = ref('')
+const status = ref('')
+
+const getPictures = async () => {
+  try {
+    const response = await fetch('https://api.thecatapi.com/v1/images/search');
+    const data = await response.json();
+
+    image.value = data[0]["url"];
+  } catch(e) {
+      message.value = e.message;
+      status.value = "error";
+  }
+}
+
+onMounted(() => {
+  getPictures();
+})
 </script>
 
 <template>
+  <Message :message="message" :status="status" />
   <main class="linear h-screen w-screen flex justify-center items-center">
     <div class="max-w-[520px] w-full p-[20px] bg-[var(--cold-turkey)] flex-col items-center rounded-[24px] shadow-lg">
-        <img src="../assets/images/cat.jpeg" alt="" class="w-full max-h-[400px] object-cover mb-20px rounded-[18px] shadow-xs" />
+        <img :src="image" alt="" class="w-full h-[400px] object-cover mb-20px rounded-[18px] shadow-xs" />
       <div class="flex justify-center items-center mt-[20px]">
         <SoundButton />
-        <button class="h-[44px] bg-[var(--bazaar)] w-[120px] flex items-center justify-center mx-4 hover:bg-[var(--kabul)] transition duration-400 ease-in-out">
+        <button @click="getPictures" class="h-[44px] bg-[var(--bazaar)] w-[120px] flex items-center justify-center mx-4 hover:bg-[var(--kabul)] transition duration-400 ease-in-out">
           <img src="../assets/images/refresh.png" alt="refresh" class="w-[20px] h-[20px]" />
         </button>
-        <button class="h-[44px] bg-[var(--bazaar)] w-[44px] flex items-center justify-center rounded-full hover:bg-[var(--kabul)] transtiion duration-400 ease-in-out">
+        <button class="h-[44px] bg-[var(--bazaar)] w-[44px] flex items-center justify-center rounded-full hover:bg-[var(--kabul)] transition duration-400 ease-in-out">
           <img src="../assets/images/star.png" alt="star" class="w-[20px] h-[20px]" />
         </button>
       </div>
