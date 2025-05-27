959cb9c22494662f2e04fd0a721619dfca47a0c5
k4ik <kaikleal1234@gmail.com>
2025-05-27 17:59:34 -0300

    feat: create getImages function

--- frontend/src/views/FavouriteView.vue
+++ frontend/src/views/FavouriteView.vue
@@ -1,5 +1,38 @@
 <script setup>
-  
+import { ref, onMounted } from 'vue';
+import Message from '../components/Message.vue';
+const images = ref([]);
+const message = ref('');
+const status = ref('');
+
+const getImages = async () => {
+  try {
+    const response = await fetch(`http://localhost:8000/api/image/saved/`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+
+    if (!response.ok) {
+      message.value = "Failed to get images";
+      status.value = "error";
+    }
+
+    const data = await response.json();
+    images.value = data;
+  } catch (e) {
+    message.value = e.message;
+    status.value = "error";
+  }
+}
 </script>
 
-<template></template>
+<template>
+  <main>
+    <h1>Your Saved Pictures</h1>
+    <div>
+      <img v-for="(image, id) in images" :src="image" :alt="id" />
+    </div>
+  </main>
+</template>
